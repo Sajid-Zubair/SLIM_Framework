276a8cab1b57f327bbc0f59afa9b8a40cbf9ec32
Sajid-Zubair <sajid10zubair@gmail.com>
2026-06-11 14:27:44 +0530

    Add gitignore, vscode config, and slim_test project

---
+++ slim_test/main.cpp
@@ -0,0 +1,26 @@
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/IR/LLVMContext.h"
+#include "slim/IR.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        llvm::errs() << "Usage: ./slim_test <file.slim.ll>\n";
+        return 1;
+    }
+
+    llvm::LLVMContext context;
+    llvm::SMDiagnostic smDiagnostic;
+
+    std::unique_ptr<llvm::Module> module =
+        llvm::parseIRFile(argv[1], smDiagnostic, context);
+
+    if (!module) {
+        smDiagnostic.print(argv[0], llvm::errs());
+        return 1;
+    }
+
+    slim::IR *slimIR = new slim::IR(module);
+    slimIR->dumpIR();
+
+    return 0;
+}
---
+++ slim_test/test.c
@@ -0,0 +1,9 @@
+int u, v;
+int *x, *y;
+
+void foo() {
+    x = &u;
+    y = x;
+    int a = 5;
+    int b = a + 3;
+}
